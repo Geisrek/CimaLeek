038c1c3a03cf0f1f520b61e48c08cac2655257d9
Rafik Hijazy <rafikhz90@gmail.com>
2024-11-04 21:54:36 +0200

    crerate  dashboard  page

--- actions/signinAct.php
+++ actions/signinAct.php
@@ -1,1 +1,12 @@
 <?php
+require_once("../connection.php");
+if(isset($_POST["email"])&&isset($_POST["password"])){
+    echo "ok";
+    $sql="SELECT * FROM user WHERE email=:email AND password=:password";
+    $stmt=$pdo->prepare($sql);
+    $stmt->bindParam("email",$_POST["email"]);
+    $stmt->bindParam("password",$_POST["password"]);
+    $stmt->execute();
+    $user=$stmt->fetch(PDO::FETCH_ASSOC);
+    print_r($user);
+}
